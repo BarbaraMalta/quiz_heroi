576fdacb9ea70690fcfdee9dcb188b7cff72d3d4
BarbaraMalta <bmaltamoraes@gmail.com>
2026-05-04 11:32:18 -0300

    addimg

--- app/src/main/java/com/example/qualheroivceh/Resultactivity.kt
+++ app/src/main/java/com/example/qualheroivceh/Resultactivity.kt
@@ -1,40 +1,66 @@
 package com.example.qualheroivceh
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import android.widget.Button
+import androidx.core.net.toUri
+import android.view.View
+import android.widget.ImageView
+
 class Resultactivity : AppCompatActivity() {
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.resultactivity)
 
-            val r1 = intent.getStringExtra("resposta1")
-            val r2 = intent.getStringExtra("resposta2")
-            val r3 = intent.getStringExtra("resposta3")
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.resultactivity)
 
-            val lista = listOf(r1, r2, r3)
+        val r1 = intent.getStringExtra("resposta1")
+        val r2 = intent.getStringExtra("resposta2")
+        val r3 = intent.getStringExtra("resposta3")
 
-            val resultado = lista.groupingBy { it }.eachCount().maxByOrNull { it.value }?.key
+        val lista = listOf(r1, r2, r3)
 
-            val txt = findViewById<TextView>(R.id.txtResultado)
+        val resultado = lista
+            .filterNotNull()
+            .groupingBy { it }
+            .eachCount()
+            .maxByOrNull { it.value }
+            ?.key
 
-            val nomeFinal = when(resultado){
-                "batman" -> "Batman 🦇"
-                "homem_aranha" -> "Homem-Aranha 🕷️"
-                "capitao" -> "Capitão América 🛡️"
-                else -> "Indefinido"
-            }
-            val btnVoltar = findViewById<Button>(R.id.btnVoltar)
+        val txt = findViewById<TextView>(R.id.txtResultado)
+        val imageView = findViewById<ImageView>(R.id.imagemHeroi)
 
-            btnVoltar.setOnClickListener {
-                val intent = Intent(this, Activity1::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
-                startActivity(intent)
-                finish()
-            }
+        val imagemRes: Int?
 
-            txt.text = getString(R.string.resultado, nomeFinal)
+        when (resultado) {
+            "batman" -> imagemRes = R.drawable.batman
+            "homem_aranha" -> imagemRes = R.drawable.homemaranha
+            "capitao" -> imagemRes = R.drawable.capitaoamerica
+            else -> imagemRes = R.drawable.erro
+        }
+
+        imageView.setImageResource(imagemRes)
+        imageView.visibility = View.VISIBLE
+
+        val nomeFinal = when (resultado) {
+            "batman" -> "Batman"
+            "homem_aranha" -> "Homem-Aranha"
+            "capitao" -> "Capitão América"
+            else -> "Indefinido"
+        }
+
+        txt.text = nomeFinal
+
+
+        findViewById<Button>(R.id.btn_browser).setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                "https://github.com/BarbaraMalta/quiz_heroi".toUri()
+            )
+            startActivity(intent)
         }
     }
+}
